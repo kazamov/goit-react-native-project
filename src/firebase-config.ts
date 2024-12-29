db5bbdbbf686a2550ad2799f0ea9e77b47b342c2
Zakir Nuriiev <zakir.nuriiev@innervate.com>
2024-12-29 20:59:52 +0200

    create a new firebase project

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: 'AIzaSyBP4LBL27Ghv66U04EmkyGc0kF1jy9vrWg',
-    authDomain: 'go-it-project-cf27c.firebaseapp.com',
-    projectId: 'go-it-project-cf27c',
-    storageBucket: 'go-it-project-cf27c.firebasestorage.app',
-    messagingSenderId: '126921498286',
-    appId: '1:126921498286:web:fb5f037c979a39f5ecd73f',
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
